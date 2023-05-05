23fee964435836a8e8f9e81cf4a7bc6a83130e96
IGNACIO MATIAS LOPEZ ABRAHAM <46872351@ort>
2023-05-05 10:03:09 -0300

    finished index

--- index.js
+++ index.js
@@ -1,2 +1,56 @@
+import express from 'express';
+import cors from 'cors';
+import {PizzaService} from './src/services/pizzas-services.js';
+
+const app = express();
+const port = 3000;
 app.use(express.json());
 app.use(cors());
+
+app.listen(port, () => {
+    console.log(`Escuchando al puerto ${port}`)
+})
+
+
+app.get('/pizza/:id', async (req,res) => {
+    const pizza = await PizzaService.getById(req.params.id)
+    res.status(200).send(pizza)
+})
+
+app.get('/pizza', async (req, res) => {
+    const pizza = await PizzaService.getAll()
+    res.status(200).send(pizza)
+})
+
+app.post('/pizza', async (req, res) => {
+    console.log("en post, req:", req)
+    try {
+        await PizzaService.insert(req.body)
+        res.status(200).json({message: 'Pizza creada'});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error:  'Fallo el insert'});
+    }
+})
+
+app.put('/pizza', async (req, res) => {
+    try{
+        await PizzaService.update(req.body)
+        res.status(200).json({message: 'Pizza actualizada'});
+
+    }catch(error){
+        console.error(error);
+        res.status(500).json({error: 'Fallo el update'});
+    }
+})
+
+app.delete('/pizza/:id', async (req, res) => {
+    try {
+        let lol = await PizzaService.deleteById(req.params.id)
+        console.log(lol);
+        res.status(200).json({message: 'Pizza eliminada !!!'});
+    } catch(error) {
+        console.error(error);
+        res.status(500).json({error: 'Fallo el delete'});
+    }
+})
--- src/services/pizzas-services.js
+++ src/services/pizzas-services.js
@@ -1,4 +1,4 @@
-import config from './dbconfig.js';
+import config from '../../dbconfig.js';
 import sql from 'mssql';
 
 export class PizzaService {
@@ -31,47 +31,44 @@ export class PizzaService {
     }
 
     static insert = async(pizza) => {
-
-        const {nombre, libreGluten, importe, descripcion} = pizza;
-        console.log("name", nombre)
+        console.log(pizza);
+        const {Id, Nombre, LibreGluten, Importe, Descripcion} = pizza;
         let pool = await sql.connect(config)
         const request = new sql.Request(pool);
 
         request
-        .input('nombre', sql.NVarChar(50), nombre)
-        .input('libreGluten', sql.Bit, libreGluten)
-        .input('importe', sql.Money, importe)
-        .input('descripcion', sql.NVarChar(200), descripcion)
-        .query('INSERT INTO Pizzas (Nombre, LibreGluten, Importe, Descripcion) VALUES (@nombre, @libreGluten, @importe, @descripcion)')
+        .input('Nombre', sql.NVarChar(50), Nombre)
+        .input('LibreGluten', sql.Bit, LibreGluten)
+        .input('Importe', sql.Float, Importe)
+        .input('Descripcion', sql.NVarChar(200), Descripcion)
+        .query('INSERT INTO Pizzas (Nombre, LibreGluten, Importe, Descripcion) VALUES (@Nombre, @LibreGluten, @Importe, @Descripcion)')
 
     }
 
     static update = async(pizza) => {
-
-        const {id, nombre, libreGluten, importe, descripcion} = pizza;
-        console.log("name", nombre)
+        const {Id, Nombre, LibreGluten, Importe, Descripcion} = pizza;
         let pool = await sql.connect(config)
         const request = new sql.Request(pool);
 
         request
-        .input('id', sql.Int, id)
-        .input('nombre', sql.NVarChar(50), nombre)
-        .input('libreGluten', sql.Bit, libreGluten)
-        .input('importe', sql.Money, importe)
-        .input('descripcion', sql.NVarChar(200), descripcion)
-        .query('UPDATE Pizzas SET Nombre = @nombre, LibreGluten = @libreGluten, Importe = @importe, Descripcion = @descripcion WHERE Id = @id ')
-
-
+        .input('Id', sql.Int, Id)
+        .input('Nombre', sql.NVarChar(50), Nombre)
+        .input('LibreGluten', sql.Bit, LibreGluten)
+        .input('Importe', sql.Float, Importe)
+        .input('Descripcion', sql.NVarChar(200), Descripcion)
+        .query('UPDATE Pizzas SET Nombre = @Nombre, LibreGluten = @LibreGluten, Importe = @Importe, Descripcion = @Descripcion WHERE Id = @Id')
     }
 
     static deleteById = async (id) => {
+        let result = null;
         try{
             let pool = await sql.connect(config);
-            let result = await pool.request()
+            result = await pool.request()
                 .input('pId', sql.Int, id)
-                .query('DELETE * FROM Pizzas WHERE id = @pId');
+                .query('DELETE FROM Pizzas WHERE id = @pId');
         }catch (error){
             console.log(error);
         }
+        return result;
     }
 }
